3300744c9045de6cf2d424c23513b91cb68447b4
andreamutti02 <andrea.mutti@raceup.it>
2024-11-06 15:13:06 +0100

    Change from h5 to f4

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ use global_allocator::init_heap;
 use panic_halt as _; 
 use cortex_m_rt::entry;
 #[allow(unused)]
-use stm32h5xx_hal;
+use stm32f4xx_hal::{pac, prelude::*, gpio};
+
+#[allow(unused)]
+use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
     init_heap();
-
+    
     loop {
         // your code goes here
     }
