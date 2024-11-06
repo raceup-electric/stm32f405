e0c47636475098b31283dfc45ac87d4786f38d6e
andreamutti02 <andrea.mutti@raceup.it>
2024-11-06 15:48:33 +0100

    Read from Serial

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use global_allocator::init_heap;
 use panic_halt as _; 
 use cortex_m_rt::entry;
 #[allow(unused)]
-use stm32f4xx_hal::{pac, prelude::*, gpio};
+use stm32f4xx_hal::{pac, prelude::*, gpio, uart::Serial};
 
 #[allow(unused)]
 use core::fmt::Write;
@@ -15,8 +15,31 @@ use core::fmt::Write;
 #[entry]
 fn main() -> ! {
     init_heap();
+
+    let dp = pac::Peripherals::take().unwrap();
     
+    let gpioa = dp.GPIOA.split();
+
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.use_hse(25.MHz()).freeze();
+
+    let mut delay = dp.TIM1.delay_ms(&clocks);
+
+    let tx_pin = gpioa.pa9;
+
+    let _rx_pin = gpioa.pa10;
+
+    let mut tx: stm32f4xx_hal::uart::Tx<pac::USART1, u8> = Serial::tx(dp.USART1, tx_pin, 115200.bps(), &clocks).unwrap();
+
+    //let mut rx: stm32f4xx_hal::uart::Rx<pac::USART1, u8> = Serial::rx(dp.USART1, rx_pin, 115200.bps(), &clocks).unwrap();
+
+    let mut value: u8 = 0;
+
     loop {
-        // your code goes here
+        // print some value every 500 ms, value will overflow after 255
+        writeln!(tx, "value: {value:02}\r").unwrap();
+        value = value.wrapping_add(1);
+        delay.delay(2.secs())
     }
 }
