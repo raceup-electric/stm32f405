ffc9ecbbdd8d85a17d5a58b72ffb7f19a349d01a
andreamutti02 <andrea.mutti@raceup.it>
2024-11-16 13:03:10 +0100

    Blink + Serial

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ use stm32f4xx_hal::{pac, prelude::*, gpio, uart::Serial};
 #[allow(unused)]
 use core::fmt::Write;
 
+extern crate alloc;
+#[allow(unused)]
+use alloc::vec::Vec;
+
 #[entry]
 fn main() -> ! {
     init_heap();
@@ -22,9 +26,15 @@ fn main() -> ! {
 
     let rcc = dp.RCC.constrain();
 
+    let _gpiob = dp.GPIOB.split();
+
+    let gpioc = dp.GPIOC.split();
+
     let clocks = rcc.cfgr.use_hse(25.MHz()).freeze();
 
-    let mut delay = dp.TIM1.delay_ms(&clocks);
+    let mut _delay = dp.TIM1.delay_ms(&clocks);
+
+    let mut led = gpioc.pc12.into_push_pull_output();
 
     let tx_pin = gpioa.pa9;
 
@@ -40,6 +50,12 @@ fn main() -> ! {
         // print some value every 500 ms, value will overflow after 255
         writeln!(tx, "value: {value:02}\r").unwrap();
         value = value.wrapping_add(1);
-        delay.delay(2.secs())
+        
+        for _ in 0..10_000 {
+            led.set_high();
+        }
+        for _ in 0..10_000 {
+            led.set_low();
+        }
     }
 }
