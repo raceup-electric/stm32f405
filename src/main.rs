00b1a753f36f6114f959eb31a5ba4d74e7a1f2e4
Alberto Damo <alberto.damo@gmail.com>
2024-12-18 15:58:55 +0100

    achieved compilation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-#![no_std]
-#![no_main]
+use embassy_executor::{Executor, Spawner};
+use static_cell::StaticCell;
 
-use embassy_executor::Spawner;
-#[allow(unused_imports)]
-use embassy_futures::join::join;
-#[allow(unused_imports)]
-use embassy_stm32::{bind_interrupts, peripherals, usb, Config};
-use defmt::info;
-use {defmt_rtt as _, panic_probe as _};
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    info!("Run")    
+#[embassy_executor::task]
+async fn main_taks(spawner: Spawner){
+}
+
+fn main() {
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner|{
+        spawner.spawn(main_taks(spawner)).unwrap();
+    });
 }
