09fb13739354d4b3643c9942e97dee90379304ad
andreamutti02 <andrea.mutti@raceup.it>
2025-04-19 10:12:16 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,23 +29,27 @@ async fn main(_spawner: Spawner) {
     {
         use embassy_stm32::rcc::*;
         config.rcc.hse = Some(Hse {
-            freq: Hertz(12_000_000), // Set to 12 MHz
+            freq: Hertz(12_000_000),
             mode: HseMode::Oscillator,
         });
+
         config.rcc.pll_src = PllSource::HSE;
         config.rcc.pll = Some(Pll {
-            prediv: PllPreDiv::DIV3, // 12 MHz / 3 = 4 MHz
-            mul: PllMul::MUL120,    // 4 MHz * 120 = 480 MHz (VCO frequency)
-            divp: Some(PllPDiv::DIV4), // 480 MHz / 4 = 120 MHz (System Clock)
-            divq: Some(PllQDiv::DIV10), // 480 MHz / 10 = 48 MHz (USB Clock)
+            prediv: PllPreDiv::DIV6,
+            mul: PllMul::MUL168,
+            divp: Some(PllPDiv::DIV2),
+            divq: Some(PllQDiv::DIV7),
             divr: None,
         });
-        config.rcc.ahb_pre = AHBPrescaler::DIV1;  // 120 MHz
-        config.rcc.apb1_pre = APBPrescaler::DIV4; // 120 MHz / 4 = 30 MHz
-        config.rcc.apb2_pre = APBPrescaler::DIV2; // 120 MHz / 2 = 60 MHz
+
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV4;
+        config.rcc.apb2_pre = APBPrescaler::DIV2;
+
         config.rcc.sys = Sysclk::PLL1_P;
         config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q;
     }
+
     let p = embassy_stm32::init(config);
 
     // Create the driver, from the HAL.
